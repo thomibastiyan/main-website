22416e9d73b2b6432c129794847de176bff18631
daffakurnia11 <72376750+daffakurnia11@users.noreply.github.com>
2021-10-12 16:41:05 +0700

    Creating Section "What is EPW?"

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -3,21 +3,19 @@
 @section('content')
     
 {{-- Buat KIKIK --}}
-<section id="epc">
-  <div class="container epc">
-    <div class="epc-header">
-      <img src="/img/icon-epc.png" class="epc-icon" alt="logo-epc">
-      <h2 class="epc-title text-end">EPC</h2>
-    </div>
-    <div class="epc-desc text-center text-center">
-      <p>EPC atau Engineering Physics Challenge,  merupakan sebuah kompetisi ilmiah tahunan dalam bidang Fisika yang diikuti oleh siswa/siswi SMA sederajat. Kegiatan tersebut diselenggarakan oleh Departemen. Teknik Fisika ITS yang bertujuan agar siswa/siswi SMA dapat mengenal ilmu-ilmu fisika dalam bidang teknik terutama lima bidang minat teknik fisika.
-      </p>
-    </div>
-    <div class="text-center">
-      <a href="" class="epc-btn">Detail</a>
-    </div>
+<section id="main-definition">
+  <img src="/svg/main-desc-wire.svg" class="main-definition-wire" alt="">
+  <div class="container definition-content">
+    <h1 class="definition-title text-end text-md-start">What is EPW?</h1>
+    <p class="definition-desc">
+      <span class="box"><span class="inner"></span></span>
+      EPW sebagai media pengenalan Teknik Fisika secara umum, keilmuan, keprofesian, dan perkuliahan (prestasi, lingkungan, dan sekitarnya) terhadap masyarakat umum, siswa calon mahasiswa baru, dan siswa SMA secara umum
+    </p>
+    <p class="definition-desc">
+      <span class="box"><span class="inner"></span></span>
+      EPW memberikan motivasi terhadap siswa sma untuk berkuliah dan menjadikan Teknik Fisika sebagai pilihan. EPW memberikan dampak tidak hanya pada peningkatan peminat Teknik Fisika, tetapi juga memberikan dampak bagi lingkungan sekitar.
+    </p>
   </div>
-  <img src="/img/bg-epc.png" class="epc-bg" alt="epc-nih">
 </section>
 {{-- End of Buat KIKIK --}}
 
